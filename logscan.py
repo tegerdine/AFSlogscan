e4dea0e31da0ec70e014a4322cb51e91335b989a
David Tegerdine <david@tegerdine.com>
2024-02-11 17:31:33 +0000

    Added 2nd Y-axis with amateur band labels; added a few comments

--- logscan.py
+++ logscan.py
@@ -14,24 +14,29 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 
+# define the number of callsign drop-down boxes to appear in the UI
 NUM_CALLS_SELECTABLE = 5
+
+# define the width in pixels of UI
 GUI_WIDTH = 320
 
+# function to analyse a CSV logfile for a list of callsigns
 def csv_analyse(filename, wanted_calls):
     df = pd.read_csv(filename, skiprows = 1)
     _, ax = plt.subplots(2, 1, figsize=(9, 6), sharex = True)
+    
     for target in wanted_calls:
         df_call = df.loc[(df["Entrant"] == target)]
         times = pd.to_datetime(df_call["Date"] + " " + df_call["Time"], format = "%d/%m/%y %H:%M")
-        ax[0].plot(times, df_call["SnTX"], label = target, marker = ".", linewidth = 1)
-        if "Frequency" in df_call.columns: # hack to make RSGB VHF logs also work
+        ax[0].plot(times, df_call["SnTX"], label = target, marker = ".", linewidth = 0.5)
+        if "Frequency" in df_call.columns: # hack to make RSGB VHF logs sort-of work too
             Freq_field="Frequency"
             y_label = "Frequency / kHz"
         else:
             Freq_field="Band"
             y_label = "Band / m"
-        ax[1].plot(times, df_call[Freq_field], label = target, marker = ".", linewidth = 1)
-
+        ax[1].plot(times, df_call[Freq_field], label = target, marker = ".", linewidth = 0.5)
+    
     ax[0].xaxis.set_major_formatter(DateFormatter("%H:%M"))
     ax[0].set_xlabel("Time")
     ax[0].set_title(f"{filename}", fontsize = 8, wrap = True)
@@ -40,20 +45,25 @@ def csv_analyse(filename, wanted_calls):
     ax[1].set_ylabel(y_label)
     ax[0].grid()
     ax[1].grid()
+    
+    if Freq_field == "Frequency": # colour and label amateur HF contest bands
+        ham_bands=[[1800,1999,'160m'], [3500,3799,'80m'], [7000,7199,'40m'], [14000,14349,'20m'], [21000,21449,'15m'], [28000,28999,'10m']]
+        for band in ham_bands:
+            ax[1].axhspan(band[0], band[1], facecolor='gold', alpha=0.5)
+            _, xmax = ax[1].get_xlim()
+            ax[1].text(xmax, band[0], band[2], fontsize=11, va='center')
+   
     plt.tight_layout()
-    plt.subplots_adjust(hspace = 0)
+    plt.subplots_adjust(hspace = 0.1)
     plt.show()
     return
 
-
+# function to manage the tkinter GUI
 def show_gui():
-    global call_list
     root = tk.Tk()
     root.title("LogScan")
     root.geometry(f"{GUI_WIDTH}x{100+(NUM_CALLS_SELECTABLE*26)}")
     root.resizable(False, False)
-
-    call_list = []
 
     def open_file():
         newfile = filedialog.askopenfile(
@@ -64,7 +74,7 @@ def show_gui():
             cur_filename.set(newfile.name)
             df = pd.read_csv(newfile.name, skiprows = 1)
             call_list = list(df.Entrant.unique())
-            call_list.insert(0, "")  # add a blank call to allow de-selection
+            call_list.insert(0, "")  # add a blank call to allow de-selection in UI
             for i in range(NUM_CALLS_SELECTABLE):
                 combo[i]["values"] = call_list
                 combo[i].set("")
